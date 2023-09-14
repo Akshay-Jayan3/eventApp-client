e2c2f14e74b5c877d561f130dc90dee1bb49e663
Akshay_jayan <akshayjayan321@gmail.com>
2023-09-14 21:05:30 +0530

    add change in login tag line

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -4,37 +4,45 @@ import styles from "./styles.module.scss";
 const LoginCard = ({ credentials, handleLogin, handleInputChange }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.welcome}>
-        <h2>Welcome Back !</h2>
-        <p>Effortless Event Planning Starts Here - Sign in Now!</p>
+      <div className={styles.wrapper}>
+        <div className={styles.welcome}>
+          <h2>Welcome Back !</h2>
+          <p>Please enter your details to sign in.</p>
+        </div>
+        <div>
+          <form onSubmit={handleLogin}>
+            <div>
+              <input
+                className={styles.inputContainer}
+                type="text"
+                name="username"
+                placeholder="username"
+                value={credentials.username}
+                onChange={handleInputChange}
+              />
+            </div>
+            <br />
+            <div>
+              <input
+                className={styles.inputContainer}
+                type="password"
+                name="password"
+                placeholder="password"
+                value={credentials.password}
+                onChange={handleInputChange}
+              />
+            </div>
+            <br />
+            <button type="submit">Login</button>
+            <div className={styles.signup}>
+              <p>Don't have an account ?</p>
+              <span>
+                <a href="/signup">Sign up</a>
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
-
-      <form onSubmit={handleLogin}>
-        <div>
-          <input
-            className={styles.inputContainer}
-            type="text"
-            name="username"
-            placeholder="username"
-            value={credentials.username}
-            onChange={handleInputChange}
-          />
-        </div>
-        <br />
-        <div>
-          <input
-            className={styles.inputContainer}
-            type="password"
-            name="password"
-            placeholder="password"
-            value={credentials.password}
-            onChange={handleInputChange}
-          />
-        </div>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-      <div className={styles.signup}><p>Don't have an account ?</p><span><a href="/signup">Sign up</a></span></div>
     </div>
   );
 };
