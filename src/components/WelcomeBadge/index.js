e60a9b3f88f0054955ecf28b1f2966e619dcb0b2
Milan OS <milan.s@thinkpalm.com>
2023-09-18 12:10:27 +0530

    Dashboard changes

--- src/components/WelcomeBadge/index.js
+++ src/components/WelcomeBadge/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useContext} from "react";
+import { UserContext } from "../../contexts/authContext";
 import styles from "./styles.module.scss";
 const WelcomeBadge = ({ Page }) => {
+  const { user} = useContext(UserContext);
   return (
     <div
       className={styles.badgeContainer}
@@ -9,16 +11,27 @@ const WelcomeBadge = ({ Page }) => {
           Page === "UpcomingEvents"
             ? "linear-gradient(to right, #e91e63, #9b59b6)"
             : Page === "PastEvents"
-            ? "linear-gradient(to right, #e67e22, #e74c3c, #e91e63)"
-            : Page === "UpcomingBirthday"
-            ? "linear-gradient(to right, #27ae60, #3498db)"
-            : null,
+              ? "linear-gradient(to right, #e67e22, #e74c3c, #e91e63)"
+              : Page === "UpcomingBirthday"
+                ? "linear-gradient(to right, #27ae60, #3498db)"
+                : Page === "Dashboard"
+                  ? "linear-gradient(to right, #FD5900, #FD5401)"
+                  : null
       }}
-    >
-      <p className={styles.mainTag}>Timeless Memories: Explore Past Events !</p>
-      <p className={styles.subTag}>
-        The past comes alive in our Past Events Gallery.
-      </p>
+    > {Page === "Dashboard" && (
+      <div>
+        <p className={styles.mainTag}>Welcome Back , {user?.full_name}</p>
+        <p className={styles.subTag}>Today you have 6 upcoming events</p>
+      </div>)}
+      {Page === "PastEvents" && (
+        <div>
+          <p className={styles.mainTag}>Timeless Memories: Explore Past Events !</p>
+          <p className={styles.subTag}>
+            The past comes alive in our Past Events Gallery.
+          </p>
+        </div>)}
+
+
     </div>
   );
 };
